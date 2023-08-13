5122f0bf2ffb79a8610d58bd7cf0441da95ac6a1
netstermonir <monirhossain6163@gmail.com>
2023-08-13 10:14:00 +0600

    localstorage

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import {
+    faCalendar,
+    faDollar,
+    faEnvelope,
+    faMapMarkerAlt,
+    faPhoneSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const AppliedJob = () => {
+    //get the data from local storage
+    const jobData = JSON.parse(localStorage.getItem("jobData"));
+
     return (
         <div>
-            applied job
+            {jobData?.map((job, index) => (
+                <>
+                    <div key={index} className="bg-[#F4F1FF] p-8 rounded">
+                        <h3 className="text-xl font-bold">Job Details</h3>
+                        <hr className="my-4" />
+                        <p>
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faDollar} />
+                            </span>{" "}
+                            {job?.salary}
+                        </p>
+                        <p className="mt-2">
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faCalendar} />
+                            </span>{" "}
+                            {job?.position}
+                        </p>
+                        <h3 className="text-xl font-bold mt-4">
+                            Contact Information:
+                        </h3>
+                        <hr className="my-4" />
+                        <p>
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faPhoneSquare} />
+                            </span>{" "}
+                            {job?.phone}
+                        </p>
+                        <p className="mt-2">
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faEnvelope} />
+                            </span>{" "}
+                            {job?.email}
+                        </p>
+                        <p className="mt-2">
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faMapMarkerAlt} />
+                            </span>{" "}
+                            {job?.location}
+                        </p>
+                    </div>
+                </>
+            ))}
         </div>
     );
 };
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -10,87 +10,114 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const JobDetails = () => {
-  const { id } = useParams();
-  const [jobs, setJobs] = useState([]);
+    const { id } = useParams();
+    const [jobs, setJobs] = useState([]);
 
-  //fetch the data
-  useEffect(() => {
-    fetch("jobs.json")
-      .then((res) => res.json())
-      .then((data) => setJobs(data));
-  }, []);
+    //fetch the data
+    useEffect(() => {
+        fetch("jobs.json")
+            .then((res) => res.json())
+            .then((data) => setJobs(data));
+    }, []);
 
-  //filter the data
-  const job = jobs?.find((job) => job.id === id);
-  return (
-    <div>
-      <div>
-        <h2 className="text-3xl bg-[#F9F9FF] font-medium text-center py-24">
-          Job Details
-        </h2>
-      </div>
-      <div className="mx-auto max-w-screen-xl py-20 mlg:flex lg:justify-between mx-4">
-        <div className="space-y-4 mb-10">
-          <p className="text-lg text-gray-500">
-            <span className="text-gray-950 font-bold">Job Description: </span>
-            {job?.jobDescription}
-          </p>
-          <p className="text-lg text-gray-500">
-            <span className="text-gray-950 font-bold">Job Responsiblity: </span>
-            {job?.jobResponsibility}
-          </p>
-          <p className="text-gray-950 font-bold">Educational Requirments:</p>
-          <p className="text-lg text-gray-500">{job?.educationRequirements}</p>
-          <p className="text-gray-950 font-bold">Experiences:</p>
-          <p className="text-lg text-gray-500">{job?.experience}</p>
-        </div>
+    //filter the data
+    const job = jobs?.find((job) => job.id === id);
+
+    const setLocalStorage = () => {
+        const previousData = JSON.parse(localStorage.getItem("jobData")) || [];
+        const newData = [...previousData, job];
+        localStorage.setItem("jobData", JSON.stringify(newData));
+    };
+    return (
         <div>
-          <div className="bg-[#F4F1FF] p-8 rounded">
-          <h3 className="text-xl font-bold">Job Details</h3>
-          <hr className="my-4" />
-          <p>
-            <span className="text-cyan-600">
-              <FontAwesomeIcon icon={faDollar} />
-            </span>{" "}
-            <span className="font-bold">Salary: </span>
-            <span className="text-gray-500">{job?.salary}(per month)</span>
-          </p>
-          <p className="mt-2">
-            <span className="text-cyan-600">
-              <FontAwesomeIcon icon={faCalendar} />
-            </span>{" "}
-            <span className="font-bold">Job Title: </span>
-            <span className="text-gray-500">{job?.position}</span>
-          </p>
-          <h3 className="text-xl font-bold mt-4">Contact Information:</h3>
-          <hr className="my-4" />
-          <p>
-            <span className="text-cyan-600">
-              <FontAwesomeIcon icon={faPhone} />
-            </span>{" "}
-            <span className="font-bold">Phone: </span>
-            <span className="text-gray-500">{job?.phone}</span>
-          </p>
-          <p className="mt-2">
-            <span className="text-cyan-600">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </span>{" "}
-            <span className="font-bold">E-mail: </span>
-            <span className="text-gray-500">{job?.email}</span>
-          </p>
-          <p className="mt-2">
-            <span className="text-cyan-600">
-              <FontAwesomeIcon icon={faLocation} />
-            </span>{" "}
-            <span className="font-bold">Address: </span>
-            <span className="text-gray-500">{job?.address}</span>
-          </p>
-          </div>
-          <button className="text-white py-2 px-4 rounded bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500 mt-6 w-full">Apply Now</button>
+            <div>
+                <h2 className="text-3xl bg-[#F9F9FF] font-medium text-center py-24">
+                    Job Details
+                </h2>
+            </div>
+            <div className=" max-w-screen-xl py-20 mlg:flex lg:justify-between mx-4">
+                <div className="space-y-4 mb-10">
+                    <p className="text-lg text-gray-500">
+                        <span className="text-gray-950 font-bold">
+                            Job Description:{" "}
+                        </span>
+                        {job?.jobDescription}
+                    </p>
+                    <p className="text-lg text-gray-500">
+                        <span className="text-gray-950 font-bold">
+                            Job Responsiblity:{" "}
+                        </span>
+                        {job?.jobResponsibility}
+                    </p>
+                    <p className="text-gray-950 font-bold">
+                        Educational Requirments:
+                    </p>
+                    <p className="text-lg text-gray-500">
+                        {job?.educationRequirements}
+                    </p>
+                    <p className="text-gray-950 font-bold">Experiences:</p>
+                    <p className="text-lg text-gray-500">{job?.experience}</p>
+                </div>
+                <div>
+                    <div className="bg-[#F4F1FF] p-8 rounded">
+                        <h3 className="text-xl font-bold">Job Details</h3>
+                        <hr className="my-4" />
+                        <p>
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faDollar} />
+                            </span>{" "}
+                            <span className="font-bold">Salary: </span>
+                            <span className="text-gray-500">
+                                {job?.salary}(per month)
+                            </span>
+                        </p>
+                        <p className="mt-2">
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faCalendar} />
+                            </span>{" "}
+                            <span className="font-bold">Job Title: </span>
+                            <span className="text-gray-500">
+                                {job?.position}
+                            </span>
+                        </p>
+                        <h3 className="text-xl font-bold mt-4">
+                            Contact Information:
+                        </h3>
+                        <hr className="my-4" />
+                        <p>
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faPhone} />
+                            </span>{" "}
+                            <span className="font-bold">Phone: </span>
+                            <span className="text-gray-500">{job?.phone}</span>
+                        </p>
+                        <p className="mt-2">
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faEnvelope} />
+                            </span>{" "}
+                            <span className="font-bold">E-mail: </span>
+                            <span className="text-gray-500">{job?.email}</span>
+                        </p>
+                        <p className="mt-2">
+                            <span className="text-cyan-600">
+                                <FontAwesomeIcon icon={faLocation} />
+                            </span>{" "}
+                            <span className="font-bold">Address: </span>
+                            <span className="text-gray-500">
+                                {job?.address}
+                            </span>
+                        </p>
+                    </div>
+                    <button
+                        className="text-white py-2 px-4 rounded bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500 mt-6 w-full"
+                        onClick={setLocalStorage}
+                    >
+                        Apply Now
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default JobDetails;
